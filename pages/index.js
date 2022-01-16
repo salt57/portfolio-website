7cb2a277ba80b80a2df7fbbc3685ba0ca3d3a4ff
salt57 <sourishgupta02@gmail.com>
2022-01-17 01:08:53 +0530

    setup: setup tailwind

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by <Image src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <Image src="/vercel.svg" alt="Vercel" className="logo" layout="fill" />
         </a>
       </footer>
 
